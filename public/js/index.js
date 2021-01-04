853895892142baba5e577ac59f9a6d3e76cff4f1
booldook <booldook@gmail.com>
2021-01-04 12:05:41 +0900

    ...

---
+++ public/js/index.js
@@ -0,0 +1,4 @@
+// openweathermap: 02efdd64bdc14b279bc91d9247db4722 (본인거로)
+// kakao: af4cf501855e85f0b321e3766472a99d (본인거로)
+
+// 7days: https://api.openweathermap.org/data/2.5/onecall?lat=38&lon=127&appid=02efdd64bdc14b279bc91d9247db4722&units=metric&exclude=minutely,hourly
