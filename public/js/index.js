df8c9ba64a9bace962b5bf1b22b44d70282bb8d7
booldook <booldook@gmail.com>
2021-01-04 13:09:46 +0900

    ....

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,20 @@
 // 웹사이트 주소체계: https://nodejs.org/dist/latest-v15.x/docs/api/url.html
+/*
+// AJAX 통신
+var url = 'https://api.openweathermap.org/data/2.5/onecall?lat=38&lon=127&appid=02efdd64bdc14b279bc91d9247db4722&units=metric&exclude=minutely,hourly';
+function onGet(r) {
+	console.log(r);
+}
+
+$("#bt").click(function(){
+	$.get(url, onGet);
+});
+*/
+
 
 // openweathermap: 02efdd64bdc14b279bc91d9247db4722 (본인거로)
 // kakao: af4cf501855e85f0b321e3766472a99d (본인거로)
 
 // 7days: https://api.openweathermap.org/data/2.5/onecall?lat=38&lon=127&appid=02efdd64bdc14b279bc91d9247db4722&units=metric&exclude=minutely,hourly
+
+
